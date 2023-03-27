86cb3291356adde6c27d493d657344e7b6ee188c
aakashdeveloper <ahanda205@gmail.com>
2023-03-27 07:53:26 +0530

    babel

--- rediswithmongo/app.js
+++ rediswithmongo/app.js
@@ -11,6 +11,24 @@ const client = redis.createClient({
 
 app.get('/data',(req,res) => {
     const userInput = (req.query.color).trim();
+    //check in redis
+    return client.get(userInput,(err,result) => {
+        //if data is in redis
+        if(result){
+            const output = JSON.parse(result);
+            res.send(output);
+        }else{
+            // as data is not in redis fetch from mongo
+            mongo.connect(mongoUrl,(err,dc) => {
+                let dbObj = dc.db('marchnode');
+                dbObj.collection('products').find({'Color':userInput}).toArray((err,result) => {
+                    //first time save in redis
+                    client.setex(userInput,3600,JSON.stringify({source:'Redis Cache',result}))
+                    res.send({source:'MongoDB',result})
+                })
+            })
+        }
+    })
 })
 
 app.listen(port,(err)=>{
---
+++ zomatoBackend/apiDoc.md
@@ -0,0 +1,23 @@
+* Page1 (Search)
+
+> List of city
+> Restaurants wrt to city
+> List of mealType
+
+
+* Page2(Listing)
+> Restaurnats wrt to mealType
+> Restaurnats wrt to mealType + Cuisine
+> Restaurnats wrt to mealType + Cost
+
+* Page3(details)
+> Details of the restaurant
+> Menu wrt to restaurant
+
+* Page4(Order)
+> Menu details wrt to choice
+> Place Order
+
+* Page5(viewOrder)
+> List all the orders
+> List all the orders wrt to email
